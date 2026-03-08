661a6b52edc6bc2a0f8d6619dd5e03df3a313fa5
Ane <anenovlar@gmail.com>
2026-03-08 17:24:59 +0100

    New icon for tip statistics and TagOption.kt colors gone back to earlier

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "1.4.3"
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
