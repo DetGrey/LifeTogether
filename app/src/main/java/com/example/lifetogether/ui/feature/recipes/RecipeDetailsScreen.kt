9a02ae6df7c1d4e4d36e371efafa4e73518b68ba
DetGrey <anenovlar@gmail.com>
2026-05-15 20:46:33 +0200

    Recipes screen has been improved

--- app/src/main/java/com/example/lifetogether/ui/feature/recipes/RecipeDetailsScreen.kt
+++ app/src/main/java/com/example/lifetogether/ui/feature/recipes/RecipeDetailsScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,7 +34,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lifetogether.R
