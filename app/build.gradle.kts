2e3d6e6a1a5997205e37e08015a08155ed490984
Ane <anenovlar@gmail.com>
2026-09-04 17:30:48 +0200

    Updated version after new feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         minSdk = 35
         targetSdk = 37
         versionCode = 1
-        versionName = "2.1.0"
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
